b391b3d960a795394f257296a9b13500290af09f
adityaramesh2 <ramesh.aditya@ufl.edu>
2022-06-27 16:54:22 -0400

    TIVA Embedded Code

---
+++ Firmware/Navigator_RF_Controller/main.c
@@ -0,0 +1,38 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+
+//*****************************************************************************
+//
+//! \addtogroup example_list
+//! <h1>Navigator RF Controller Board</h1>
+//!
+//! Insert description
+//
+//*****************************************************************************
+
+//*****************************************************************************
+//
+// The error routine that is called if the driver library encounters an error.
+//
+//*****************************************************************************
+#ifdef DEBUG
+void
+__error__(char *pcFilename, uint32_t ui32Line)
+{
+    while(1);
+}
+#endif
+
+//*****************************************************************************
+//
+// MAIN
+//
+//*****************************************************************************
+int main(void)
+{
+
+}
